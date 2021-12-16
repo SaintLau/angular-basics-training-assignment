715af026c15f25fc7ea0c72f4c25b19b5ff1a831
Laura Santos <alsr16p@gmail.com>
2021-12-16 17:00:30 +0000

    first and last commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+//import { threadId } from 'worker_threads';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  allowDetail = false;
+  detail = [];
+
+
+  onToggleDetails(){
+    this.allowDetail = !this.allowDetail;
+    this.detail.push(this.detail.length + 1);
+  }
+}
